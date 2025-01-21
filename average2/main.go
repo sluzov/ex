ec234dc365ade1a82372d17d2f4a6ee3b2b01cee
sluzov <iamsluzov@gmail.com>
2025-01-22 01:08:00 +0300

    Слайсы

---
+++ average2/main.go
@@ -0,0 +1,26 @@
+// average2 вычисляет среднее значение.
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	arguments := (os.Args[1:])
+	var sum float64 = 0
+	for _, argument := range arguments {
+		number, err := strconv.ParseFloat(argument, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		sum += number
+	}
+
+	sampleCount := float64(len(arguments))
+	fmt.Printf("Average: %0.2f\n", sum/sampleCount)
+
+}
--- datafile/floats.go
+++ datafile/floats.go
@@ -8,31 +8,31 @@ import (
 )
 
 // Getfloats читает значение float64 из каждой строки файла
-func GetFloats(fileName string) ([3]float64, error) {
-	var numbers [3]float64
+func GetFloats(fileName string) ([]float64, error) {
+	var numbers []float64
 
 	file, err := os.Open(fileName)
 	if err != nil {
-		return numbers, err
+		return nil, err
 	}
 
-	i := 0
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
-		numbers[i], err = strconv.ParseFloat(scanner.Text(), 64)
+		number, err := strconv.ParseFloat(scanner.Text(), 64)
 		if err != nil {
-			return numbers, err
+			return nil, err
 		}
-		i++
+
+		numbers = append(numbers, number)
 	}
 
 	err = file.Close()
 	if err != nil {
-		return numbers, err
+		return nil, err
 	}
 	if scanner.Err() != nil {
-		return numbers, scanner.Err()
+		return nil, scanner.Err()
 	}
 
 	return numbers, nil
