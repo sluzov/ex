15262742fa6083e22e89aed0ec734076a7059714
sluzov <iamsluzov@gmail.com>
2025-01-21 19:39:04 +0300

    Массивы

--- main.go
+++ main.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"fmt"
-	"sluzov/greeting" // Импортируйте ваш пакет по имени из go.mod
+	// Импортируйте ваш пакет по имени из go.mod
+	"log"
+	"sluzov/datafile"
 )
 
 func main() {
-	fmt.Println("This is main!")
-	greeting.Hello()
-	greeting.Hi()
+	numbers, err := datafile.GetFloats("C:/1Sluzov/Test1/data.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var sum float64 = 0
+
+	for _, number := range numbers {
+		sum += number
+	}
+
+	sampleCount := float64(len(numbers))
+	fmt.Printf("Average: %0.2f\n", sum/sampleCount)
 }
